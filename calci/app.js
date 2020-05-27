e0b5c543041f6932ab036c8bdbfde0d27b7195f9
Satish <satishmamilapalli@gmail.com>
2020-05-27 17:53:24 +0530

    my files

--- calci/app.js
+++ calci/app.js
@@ -1,8 +1,24 @@
 (function () {
-
+'use strict';
   angular.module('MyFirstAjs',[])
   .controller('MyController',function ($scope) {
-    $scope.name="Satish";
+    $scope.name="";
+    $scope.totalValue=0;
+
+    $scope.disp = function () {
+      var y = FindCalci($scope.name);
+      $scope.totalValue=y;
+    };
+
+    function FindCalci(String)
+    {
+       var x = 0;
+       for(var i=0;i<String.length;i++)
+       {
+       x=x+String.charCodeAt(i);
+     }
+       return x;
+    }
   });
 
 })();
